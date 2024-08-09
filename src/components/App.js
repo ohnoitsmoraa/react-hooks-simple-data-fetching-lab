79f3758b47f6566c8c3b978039d54059b755b0e0
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-08-09 11:52:50 +0300

    Modified App.js to apply a simple data fetch from an API

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App() {
+    const [dogImage, setDogImage] = useState("")
+    const [loading, setLoading] = useState(true)
+
+    useEffect (() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+            setDogImage(data.message)
+            setLoading(false)
+        })
+    }, [])
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+    return (
+        <div>
+            <img src={dogImage} alt="A Random Dog" />
+        </div>
+    )
+    }
+
+export default App
